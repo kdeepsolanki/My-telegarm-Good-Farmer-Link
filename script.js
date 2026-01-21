bdebf9c78f42d5a6f4aa80d1600254d1311eb514
kdeepsolanki <agrodastak@gmail.com>
2026-01-22 00:51:48 +0530

    Refactor button injection and update comments

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 1. Kill Switch
+    // 1. Kill Switch (Optional)
     var expiryDate = new Date('2026-02-15'); 
-    if (new Date() > expiryDate) {
-        document.body.innerHTML = '<h1 style="text-align:center;margin-top:100px;">Service Suspended</h1>';
-        return;
-    }
+    if (new Date() > expiryDate) { return; }
 
-    // 2. Button HTML
+    // 2. Buttons ka asli HTML jo wapas aayega
     var btnHTML = `
         <div class='tg-container'>
-            <a class='tg-btn tg-blue' href='https://t.me/agrodastak' target='_blank'>
-                <i class='fa-brands fa-telegram'></i> Join Telegram (Good Farmer)
-            </a>
-            <a class='tg-btn tg-orange-glass' href='https://t.me/agriculturesupervisorpyq' target='_blank'>
-                <i class='fa-brands fa-telegram'></i> Join Telegram (Agro Dastak)
-            </a>
+          <a class='tg-btn tg-blue' href='https://t.me/agrodastak'>
+              <i class='fa-brands fa-telegram'></i> Join Telegram (Good Farmer) 
+          </a>
+          <a class='tg-btn tg-orange-glass' href='https://t.me/agriculturesupervisorpyq'>
+              <i class='fa-brands fa-telegram'></i> Join Telegram (Agro Dastak) 
+          </a>
         </div>`;
 
-    // 3. Smart Positioning
-    var target = document.getElementById('target-container');
+    // 3. Auto-Injection (Jahan bhi '.container' class milegi, wahan buttons aa jayenge)
+    var target = document.querySelector('.container');
     if (target) {
-        target.innerHTML = btnHTML;
-    } else {
-        // Agar ID nahi milti, toh 'Agriculture Quizzes' wale section ke upar auto-insert karega
-        var quizSection = document.querySelector('.section-box') || document.querySelector('.main');
-        if (quizSection) {
-            quizSection.insertAdjacentHTML('beforebegin', btnHTML);
+        // Agar buttons pehle se nahi hain, tabhi insert karega
+        if (!document.querySelector('.tg-container')) {
+            target.insertAdjacentHTML('afterbegin', btnHTML);
         }
     }
 });
