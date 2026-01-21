7eb0d1970314543eaf22399aaa994cafcebd17d9
kdeepsolanki <agrodastak@gmail.com>
2026-01-22 01:02:05 +0530

    Refactor button injection logic in script.js

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-(function() {
-    function injectButtons() {
-        // 1. Kill Switch
-        var expiryDate = new Date('2026-02-15'); 
-        if (new Date() > expiryDate) return;
+document.addEventListener("DOMContentLoaded", function() {
+    // 1. Buttons ka HTML (Jo GitHub se deliver hoga)
+    var btnHTML = `
+        <div class='tg-container' id='secure-buttons'>
+          <a class='tg-btn tg-blue' href='https://t.me/agrodastak' target='_blank'>
+              <i class='fa-brands fa-telegram'></i> Join Telegram (Good Farmer) 
+          </a>
+          <a class='tg-btn tg-orange-glass' href='https://t.me/agriculturesupervisorpyq' target='_blank'>
+              <i class='fa-brands fa-telegram'></i> Join Telegram (Agro Dastak) 
+          </a>
+        </div>`;
 
-        // 2. Buttons HTML
-        var btnHTML = `
-            <div class='tg-container' id='secure-tg-box'>
-              <a class='tg-btn tg-blue' href='https://t.me/agrodastak' target='_blank'>
-                  <i class='fa-brands fa-telegram'></i> Join Telegram (Good Farmer) 
-              </a>
-              <a class='tg-btn tg-orange-glass' href='https://t.me/agriculturesupervisorpyq' target='_blank'>
-                  <i class='fa-brands fa-telegram'></i> Join Telegram (Agro Dastak) 
-              </a>
-            </div>`;
+    // 2. Sahi Position ki Talaash (Bina kisi ID ke)
+    // Ye line aapke "Agriculture Quizzes" wale box (section-box) ko dhoondti hai
+    var target = document.querySelector('.section-box.agri-style') || document.querySelector('.main-content');
 
-        // 3. Sahi Position ki Pehchan (Agriculture Quiz ke upar)
-        // Hum us section ko dhundhenge jo aapki image_b9a217.png mein line 269 par hai
-        var targetSection = document.querySelector('.section-box.agri-style') || 
-                            document.querySelector('#agri-sec') || 
-                            document.querySelector('.container > b\\:if');
-
-        if (targetSection && !document.getElementById('secure-tg-box')) {
-            targetSection.insertAdjacentHTML('beforebegin', btnHTML);
-        }
+    if (target) {
+        // Buttons ko Agriculture Quizzes ke bilkul upar insert karega
+        target.insertAdjacentHTML('beforebegin', btnHTML);
     }
-
-    // Site load hone par chalao
-    document.addEventListener("DOMContentLoaded", injectButtons);
-    // 2 second baad dobara check karega (agar dost ne delete kiya toh turant wapas layega)
-    setInterval(injectButtons, 3000);
-})();
+});
